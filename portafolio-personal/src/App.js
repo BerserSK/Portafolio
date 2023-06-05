ff6f13d0079636ef6d467a25457f49829c599e24
BerserSK <andres2003latorre@gmail.com>
2023-06-04 23:58:19 -0500

    Se termina el proyecto (faltan algunos cambios)

--- portafolio-personal/src/App.js
+++ portafolio-personal/src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
-import { Projects } from './components/projects';
+import { Projects } from './components/Projects';
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    fetch("/api")
+    .then((res) => res.json())
+    .then((data) => { console.log(data)})
+  }, [])
   return (
     <div className="App">
       <NavBar />
